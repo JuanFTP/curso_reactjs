6cb8507902b9628e8315e718e9e42c634397f83e
JuanFTP <juanftp10@gmail.com>
2021-03-10 22:05:22 -0600

    Lección 65. Creación de componente

--- calc-app/src/App.jsx
+++ calc-app/src/App.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import Button from './components/Button';
+import MathOperations from './components/MathOperations';
 import Result from './components/Result';
 import './App.css';
 
 const App = () => {
-	console.debug("Renderización de App");
+	const cHButton = text => {
+		console.debug("App clickHandlerButton", text);
+	};
+
 	return (
 		<div className='calc-base'>
-			<Result />
+			<Result value={"0"} />
+			<div className="numbers">
+				<Button type="number" text="1" clickHandler={cHButton} />
+				<Button type="number" text="2" clickHandler={cHButton} />
+				<Button type="number" text="3" clickHandler={cHButton} />
+				<Button type="number" text="4" clickHandler={cHButton} />
+				<Button type="number" text="5" clickHandler={cHButton} />
+				<Button type="number" text="6" clickHandler={cHButton} />
+				<Button type="number" text="7" clickHandler={cHButton} />
+				<Button type="number" text="8" clickHandler={cHButton} />
+				<Button type="number" text="9" clickHandler={cHButton} />
+				<Button type="number" text="0" clickHandler={cHButton} />
+			</div>
+			<MathOperations></MathOperations>
 		</div>
 	);
 };

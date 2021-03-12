eb0a7eeb56b41a82dab3207387a67b09ce815daf
JuanFTP <juanftp10@gmail.com>
2021-03-11 21:07:44 -0600

    Lección 69. Creación de componente Funcions

--- calc-app/src/App.jsx
+++ calc-app/src/App.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
 import Button from './components/Button';
+import Functions from './components/Functions';
 import MathOperations from './components/MathOperations';
 import Result from './components/Result';
 import './App.css';
 
 const App = () => {
 	const cHButton = text => {
-		console.debug("App clickHandlerButton", text);
+		console.debug("App", text);
 	};
 
 	return (
 		<div className='calc-base'>
 			<Result value={"0"} />
-			<div className="numbers">
-				<Button type="number" text="1" clickHandler={cHButton} />
-				<Button type="number" text="2" clickHandler={cHButton} />
-				<Button type="number" text="3" clickHandler={cHButton} />
-				<Button type="number" text="4" clickHandler={cHButton} />
-				<Button type="number" text="5" clickHandler={cHButton} />
-				<Button type="number" text="6" clickHandler={cHButton} />
-				<Button type="number" text="7" clickHandler={cHButton} />
-				<Button type="number" text="8" clickHandler={cHButton} />
-				<Button type="number" text="9" clickHandler={cHButton} />
-				<Button type="number" text="0" clickHandler={cHButton} />
+			<div className="grid-left">
+				<div className="numbers">
+					<Button type="number" text="1" clickHandler={cHButton} />
+					<Button type="number" text="2" clickHandler={cHButton} />
+					<Button type="number" text="3" clickHandler={cHButton} />
+					<Button type="number" text="4" clickHandler={cHButton} />
+					<Button type="number" text="5" clickHandler={cHButton} />
+					<Button type="number" text="6" clickHandler={cHButton} />
+					<Button type="number" text="7" clickHandler={cHButton} />
+					<Button type="number" text="8" clickHandler={cHButton} />
+					<Button type="number" text="9" clickHandler={cHButton} />
+					<Button type="number" text="0" clickHandler={cHButton} />
+				</div>
+				<Functions onContentClear={clear => console.debug("Clear", clear)} onDelete={del => console.debug("Del", del)} />
 			</div>
-			<MathOperations></MathOperations>
+			<MathOperations onClickOperation={operation => console.debug("Operation", operation)} onClickEqual={equal => console.debug("Equal", equal)} />
 		</div>
 	);
 };

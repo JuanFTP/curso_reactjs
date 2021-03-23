dd8680f8a14b7cc0d58513816e12695d1c8fe9e1
JuanFTP <juanftp10@gmail.com>
2021-03-22 21:04:11 -0600

    Lección 110. Build de la aplicación

--- calc-app/src/App.jsx
+++ calc-app/src/App.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
+/* eslint no-eval: 0 */
+import React, { useState } from 'react';
 import Functions from './components/Functions';
+import words from 'lodash.words';
 import Numbers from './components/Numbers';
 import MathOperations from './components/MathOperations';
 import Result from './components/Result';
 import './App.css';
 
 const App = () => {
+	const [stack, setStack] = useState("");
+	const items = words(stack, /[^-^+^*^/]+/g);
+	const value = items.length > 0 ? items[items.length - 1] : "0";
+
 	return (
 		<div className='calc-base'>
-			<Result value={"0"} />
+			<Result value={value} />
 			<div className="grid-left">
-				<Numbers onClickNumber={number => console.debug("Number", number)} />
-				<Functions onContentClear={clear => console.debug("Clear", clear)} onDelete={del => console.debug("Del", del)} />
+				<Numbers
+					onClickNumber={number => { setStack(`${stack}${number}`); }} />
+				<Functions
+					onContentClear={() => { setStack(""); }}
+					onDelete={() => { if (stack.length > 0) { const newStack = stack.substring(0, stack.length - 1); setStack(newStack); } }} />
 			</div>
-			<MathOperations onClickOperation={operation => console.debug("Operation", operation)} onClickEqual={equal => console.debug("Equal", equal)} />
+			<MathOperations
+				onClickOperation={operation => { setStack(`${stack}${operation}`); }}
+				onClickEqual={equal => { setStack(eval(stack).toString()); }} />
 		</div>
 	);
 };

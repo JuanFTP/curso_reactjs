8b5939d42b541510e0a299541f015642f58b18ec
JuanFTP <juanftp10@gmail.com>
2022-02-01 22:56:13 -0600

    Lección 503. Validaciones múltiples en fields y mejora a MyField

--- customer-app/src/components/CustomerEdit.js
+++ customer-app/src/components/CustomerEdit.js
@@ -3,6 +3,22 @@ import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
 import { setPropsAsInitial } from './../helpers/setPropsAsInitial';
 
+const isRequired = (value) => (
+	!value && "This field is required"
+);
+
+const MyField = ({ input, meta, type, label, name, placeholder }) => (
+	<div class="form-control">
+		<label htmlFor={name}>{label}</label>
+		<input {...input} type={!type ? "text" : type} placeholder={placeholder}/>
+		{meta.touched && meta.error && <span className="msg-error">{meta.error}</span>}
+	</div>
+);
+
+const isNumber = (value) => (
+	isNaN(Number(value)) && "This field should be a number"
+);
+
 const CustomerEdit = ({ name, dni, age }) => {
 	/* NOTE
 		Los datos que vengan del initialValue deben ser iguales al nombre del objeto - parámetro Field
@@ -12,18 +28,27 @@ const CustomerEdit = ({ name, dni, age }) => {
 			<h2>Editar cliente</h2>
 			<div>
 				<form action="#" className="form">
-					<div>
-						<label htmlFor="name">Nombre</label>
-						<Field name="name" component="input" type="text"></Field>
-					</div>
-					<div>
-						<label htmlFor="dni">DNI</label>
-						<Field name="dni" component="input" type="text"></Field>
-					</div>
-					<div>
-						<label htmlFor="age">Edad</label>
-						<Field name="age" component="input" type="number"></Field>
-					</div>
+					<Field name="name"
+						component={MyField}
+						type="text"
+						validate={isRequired}
+						label="Nombre"
+						placeholder="Juan Temich">
+					</Field>
+					<Field name="dni"
+						component={MyField}
+						type="text"
+						validate={[isRequired, isNumber]}
+						label="DNI"
+						placeholder="0000000000">
+					</Field>
+					<Field name="age"
+						component={MyField}
+						type="number"
+						validate={isNumber}
+						label="Edad"
+						placeholder="35">
+					</Field>
 				</form>
 			</div>
 
